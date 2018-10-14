32898bc0c32b41329dc7cbd153458924271b3916
asc5025 <albert.s.chun@gmail.com>
2018-10-14 15:00:38 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbait?
+
+  @@clickbait_words = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+
+
+  def is_clickbait?
+    if @@clickbait_words.none? { |word| word.match title }
+      errors.add(:title, "Clickbait Alert")
+    end
+  end
+
 end
